ea167fe9fec9ec1328a6e2d27403139770c7d740
Akbar30Bill <alishahverdi2010@yahoo.com>
2019-05-02 20:25:56 +0430

    BST

--- 1.py
+++ 1.py
@@ -1,25 +1,34 @@
-def sort_first(lst):
-    tmp = lst[-1]
-    lst.pop()
-    for i in range(len(lst)-1,-1,-1):
-        if tmp < lst[i]:
-            lst.insert(tmp,i)
-            return
-
-n,time = list(map(int,input().split()))
-lsblk = 0
-lst = list(map(int,input().split()))
-lst.sort()
-# print(lst)
-# for i in range(len(lst)):
-#     lst[i] = [lst[i],i]
-# print(lst)
-# lst.sort(key=lambda x:x[0])
+# def sort_first(lst):
+#     tmp = lst[-1]
+#     lst.pop()
+#     for i in range(len(lst)-1,-1,-1):
+#         if tmp < lst[i]:
+#             lst.insert(tmp,i)
+#             return
+# n,time = list(map(int,input().split()))
+# lsblk = 0
+# lst = list(map(int,input().split()))
 # lst.sort()
-# print(lst)
-for i in range(time):
-    # print(lst)
-    lsblk += lst[-1]
-    lst[-1] = int(lst[-1]/2)
-    sort_first(lst)
-print(lsblk)
+# for i in range(time):
+#     # print(lst)
+#     lsblk += lst[-1]
+#     lst[-1] = int(lst[-1]/2)
+#     sort_first(lst)
+# print(lsblk)
+#-----------------------------------------------------------
+
+class node:
+    def __init__(this, parrent, data):
+        this.data = data
+        this.parrent = parrent
+    def insert(this, value_to_insert):
+        try:
+            if value_to_insert > this.data:
+                this.rightChild.insert(value_to_insert)
+            else:
+                this.leftChild.insert(value_to_insert)
+        except:
+            if value_to_insert > this.data:
+                this.rightChild = node(this, value_to_insert)
+            else:
+                this.leftChild  = node(this, value_to_insert)
